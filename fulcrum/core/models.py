147778301965ac55e4af79f1acc9d0826e15bfef
Soham Shirke <sohamshirke.215099@gmail.com>
2025-01-13 20:09:04 +0530

    chore: added studentID & password

--- fulcrum/core/models.py
+++ fulcrum/core/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from phonenumber_field.modelfields import *
+from django.contrib.auth.hashers import make_password
 # Create your models here.
 class Student(models.Model):
+    student_id = models.CharField(max_length=20, unique=True, default='') 
+    password = models.CharField(max_length=128, default='')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
     phone = PhoneNumberField()
+
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
--- fulcrum/core/views.py
+++ fulcrum/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.contrib import messages
+from django.contrib.auth.hashers import *
 # Create your views here.
 def Std_reg(request):
     if request.method == 'POST':
@@ -9,13 +10,16 @@ def Std_reg(request):
         lastname = data.get('last_name')
         email = data.get('email')
         phone = data.get('phone')
+        password = data.get('password')
+        student_id = data.get('student_id')
         
         if not Student.objects.filter(email=email).exists() and not Student.objects.filter(phone=phone).exists():
             student = Student.objects.create(
+                student_id = student_id,
                 first_name = firstname,
                 last_name = lastname,
                 email = email,
-                phone = phone
+                password = make_password(password)
             )
             return render(request, 'studentReg.html', {'message': 'Registration successful!'})
         else:
