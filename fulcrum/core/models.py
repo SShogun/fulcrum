915a5a6cf9d8922eed5ec3a81c71558e3eeac727
Soham Shirke <sohamshirke.215099@gmail.com>
2025-01-13 19:42:37 +0530

    feat: added student registration view

--- fulcrum/core/models.py
+++ fulcrum/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from phonenumber_field.modelfields import *
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, unique=True)
+    phone = PhoneNumberField()
--- fulcrum/core/views.py
+++ fulcrum/core/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib import messages
 # Create your views here.
+def Std_reg(request):
+    if request.method == 'POST':
+        data = request.POST
+        firstname = data.get('first_name')
+        lastname = data.get('last_name')
+        email = data.get('email')
+        phone = data.get('phone')
+        
+        if not Student.objects.filter(email=email).exists() and not Student.objects.filter(phone=phone).exists():
+            student = Student.objects.create(
+                first_name = firstname,
+                last_name = lastname,
+                email = email,
+                phone = phone
+            )
+            return render(request, 'studentReg.html', {'message': 'Registration successful!'})
+        else:
+            messages.error(request, 'Email or phone number already exists!')
+            return render(request, 'studentReg.html')
+    
+    return render(request, 'studentReg.html')
